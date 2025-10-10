a9d197ac366b565492f52e6273139eb1ea33eee0
LapSexyBeast <w0510589@campus.nscc.ca>
2025-10-10 15:57:55 -0300

    Changed some label displays

--- PawsForApplause/Models/Show.cs
+++ PawsForApplause/Models/Show.cs
@@ -8,21 +8,29 @@ namespace PawsForApplause.Models
     public class Show
     {
         public int ShowId { get; set; }
+        
         public string Name { get; set; } = string.Empty; // event's title
+        [Display(Name = "Event Description")]
         public string Description { get; set; } = string.Empty; //description
+        [Display(Name = "Event Date")]
         public DateTime Date { get; set; } //date and time
         public DateTime Created { get; set; } //date and time record created
         public DateTime LastModified { get; set; } //date and time record last modified
         public string Filename { get; set; } = string.Empty;
 
         // Foreign Keys
+        [Display(Name = "Event Owner")]
         public int UserId { get; set; } // FK to User (who created the event)
+        [Display(Name = "Event Location")]
         public int VenueId { get; set; } // FK to Venue
+        [Display(Name = "Event Type")]
         public int CategoryId { get; set; } //FK to Category
 
         //Navigation Properties
         public Category? Category { get; set; }
+        [Display(Name = "Event Owner")]
         public User? User { get; set; } // Navigation property to User
+        [Display(Name = "Event Location")]
         public Venue? Venue { get; set; } // Navigation property to Venue
         
 
